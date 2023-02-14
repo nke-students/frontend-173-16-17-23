0ef5f1e118c5a03897467c8daf719ec2b5e631d9
Samsebeaga <yano00g@yandex.ru>
2023-02-14 01:59:05 +0000

    chtoto

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>Boo, i am ghost this website...UUUUU This begans in 1999...when people wanted more and more...</div>
   </React.StrictMode>
